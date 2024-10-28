ba35413da03a25f0158754cc4921098b8169dc3c
Pang Jichen <138418274+WhaleFallOf52Hz@users.noreply.github.com>
2024-10-28 20:13:48 +0800

    Update glb2obj.py

--- glb2obj.py
+++ glb2obj.py
@@ -3,8 +3,10 @@ from utils.get_objtexture import GLB2OBJ
 from utils.folder_copy import copy_folder_contents
 import os
 import shutil
+import gc
 
 if __name__ == '__main__':
+    gc.enable()
     dir_name = os.path.dirname(os.path.abspath(__file__))
 
     glb2obj=GLB2OBJ(model_dir=dir_name + '/glb_data', save_dir=dir_name + '/wangyi_522030910147/collected_data')
